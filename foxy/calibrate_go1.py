1694056d751d8673c2f5440a06cdd047e22696cf
Break Yang <breakds@gmail.com>
2023-07-18 12:01:59 -0700

    Add physical recording

--- foxy/calibrate_go1.py
+++ foxy/calibrate_go1.py
@@ -9,9 +9,10 @@ from loguru import logger
 import mujoco
 import mujoco.viewer
 from mujoco import MjModel, MjData
+import pygame
 from go1agent import Go1Agent
-
 from foxy.mujoco_agent import MujocoAgent
+
 
 # fmt: off
 NEUTRAL_STANCE_QPOS = [
@@ -72,11 +73,12 @@ class Script(object):
 
     def statements(self) -> List[str]:
         statements: List[str] = []
+        statements.append(f"CONTROL FREQUENCY {self._control_frequency} Hz")
         for name, qpos in self._stances.items():
             statements.append(f"STANCE {name} {qpos}")
         statements.append("")
         for name, duration in self._keyframes:
-            statements.append(f"KEYFRAME {name} {duration} seconds")
+            statements.append(f"KEYFRAME {name} DURATION {duration} seconds")
         return statements
 
     def __str__(self) -> str:
@@ -105,11 +107,12 @@ class Recorder(object):
     def add_script(self, script: Script):
         self.script = f"{script}"
 
-    def add_mujoco(self, agent: MujocoAgent):
-        if self.type == "undecided":
+    def add(self, agent: MujocoAgent | Go1Agent, cmd: np.ndarray):
+        if isinstance(agent, MujocoAgent):
             self.type = "mujoco"
-        assert self.type == "mujoco"
-        self.cmd.append(agent.data.ctrl)
+        elif isinstance(agent, Go1Agent):
+            self.type = "physical"
+        self.cmd.append(cmd)
         state = agent.read()
         self.qpos.append(state.leg.q)
         self.qvel.append(state.leg.qd)
@@ -141,6 +144,23 @@ class Recorder(object):
         logger.info(f"Successfully saved recorded data to {path}")
 
 
+class PhysicalGUI(object):
+    def __init__(self):
+        pygame.init()
+        self._screen: pygame.Surface = pygame.display.set_mode((1280, 800))
+
+    def check_space_pressed(self, label: str):
+        font = pygame.font.Font(None, 36)
+        pygame.event.get()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            return True
+        text = font.render(f"Press Space to {label} ...", True, (0, 0, 0))
+        self._screen.fill("white")
+        self._screen.blit(text, (100, 400))
+        return False
+
+
 @click.group()
 def app():
     pass
@@ -155,11 +175,13 @@ def mjc(record: str):
     logger.info("Calibrating Go1 in MuJoCo ...")
     dt = 0.005
     decimation = 4
+    control_frequency = int(1.0 / decimation / dt)
+    assert control_frequency == 50
     agent = MujocoAgent(sim_dt=dt)
 
     # The following script let the robot stand for 3 seconds and squat for 3
     # seconds, back and forth.
-    script = Script()
+    script = Script(control_frequency=control_frequency)
     script.stance("stand", NEUTRAL_STANCE_QPOS)
     script.stance("squat", SLIGHT_SQUAT_QPOS)
     script.keyframe("stand", 3.0)
@@ -171,9 +193,10 @@ def mjc(record: str):
         step = 0
         while viewer.is_running():
             start = time.time()
-            agent.publish_action(script.cmd(step))
+            cmd = script.cmd(step)
+            agent.publish_action(cmd)
             for _ in range(decimation):
-                recorder.add_mujoco(agent)
+                recorder.add(agent, cmd)
                 agent.step(1)
 
             with viewer.lock():
@@ -185,9 +208,74 @@ def mjc(record: str):
     recorder.save(Path(record))
 
 
+def phsyical_soft_initialize(agent: Go1Agent):
+    logger.info("Will softly initialize the robot to stand ...")
+
+    final_goal = NEUTRAL_STANCE_QPOS
+
+    # Prepare the interpolated action (target qpos) sequence to reach
+    # the final goal qpos.
+    sensor = agent.read()
+    target_sequence = []
+    target = sensor.leg.q()
+    while np.max(np.abs(target - final_goal)) > 1e-2:
+        target += np.clip(final_goal - target, -0.03, 0.03)
+        target_sequence.append(target.copy())
+
+    # Now execute the sequence with 20 Hz control frequency
+    for target in target_sequence:
+        # Directly publish the action to agent as they are already target
+        # joint angles.
+        agent.publish_action(target)
+        time.sleep(0.05)
+
+    logger.info("Physical robot initialization done")
+
+
 @app.command()
-def physical():
+@click.option(
+    "--record",
+    default="/home/breakds/syncthing/workspace/hobot/calibrate_go1/mujoco.pkl",
+)
+def physical(record):
     logger.info("Calibrating on Physical Go1 ...")
+    dt = 0.005
+    decimation = 4
+    control_frequency = int(1.0 / decimation / dt)
+    assert control_frequency == 50
+    agent = Go1Agent(1000)  # Internal loop at 1000 Hz
+    agent.spin()
+
+    # The following script let the robot stand for 3 seconds and squat for 3
+    # seconds, back and forth.
+    script = Script(control_frequency=control_frequency)
+    script.stance("stand", NEUTRAL_STANCE_QPOS)
+    script.stance("squat", SLIGHT_SQUAT_QPOS)
+    script.keyframe("stand", 3.0)
+    script.keyframe("squat", 3.0)
+    script.log()
+
+    gui = PhysicalGUI()
+    while True:
+        if gui.check_space_pressed("start"):
+            break
+    phsyical_soft_initialize(agent)
+
+    step = 0
+    recorder = Recorder()
+    while True:
+        start = time.time()
+        cmd = script.cmd(step)
+        recorder.add(agent, cmd)
+        agent.publish_action(cmd)
+        for i in range(1, decimation):
+            time.sleep(max(start + dt * i - time.time(), 0))
+            recorder.add(agent, cmd)
+        step = step + 1
+        if gui.check_space_pressed("finish"):
+            break
+        time.sleep(max(start + dt * decimation - time.time(), 0))
+    recorder.save(Path(record))
 
 
 if __name__ == "__main__":
